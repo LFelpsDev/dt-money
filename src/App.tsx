98801cb30c47f50aa16e975e7629064a95255c4a
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-07-30 15:37:09 -0300

    adicionando a Lib React-Modal, Fazendo a Logica de Open and Close, e Compartilhando Função de Elemento Filho para Elemento Pai

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal'; // é preciso tipar ele, quando estar utilizando o TypeScript
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
+Modal.setAppElement('#root'); // para aces
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} // a função que teve ser executada quando o usario aperta esc, ou clicar fora do modal   
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
