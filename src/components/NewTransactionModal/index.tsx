20d7f9f65c1b5b1942d62c836eecc31cfb15caa4
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-07-30 16:01:03 -0300

    Create Estrutura Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -7,15 +7,37 @@ interface NewTransactionModalProps {
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose,}: NewTransactionModalProps){
+export function NewTransactionModal({ isOpen, onRequestClose, }: NewTransactionModalProps) {
   return (
     <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose} // a função que teve ser executada quando o usario aperta esc, ou clicar fora do modal   
-      >
-        <Container>
-          
-        </Container>
-      </Modal>
+      isOpen={isOpen}
+      onRequestClose={onRequestClose} // a função que teve ser executada quando o usario aperta esc, ou clicar fora do modal   
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input
+          placeholder="Titulo"
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+
+        <button type="submit">
+          Cadastrar
+        </button>
+
+
+      </Container>
+    </Modal>
   )
 }
