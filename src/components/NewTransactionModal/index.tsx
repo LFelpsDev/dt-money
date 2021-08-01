08582632f7396cfeae93cdfef189e234c94110a8
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-01 20:54:10 -0300

    Finalizando a Inserção, colocando as Transactions em Tela

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -23,15 +23,21 @@ export function NewTransactionModal({ isOpen, onRequestClose, }: NewTransactionM
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent){
+ async function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
 
-    createTransactions({
+   await createTransactions({
       title,
       amout,
       category,
       type,
     })
+
+    setTitle('');
+    setAmout(0);
+    setCategory('')
+    setType('deposit');
+    onRequestClose();
     
   }
 
